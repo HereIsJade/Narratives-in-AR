9c0e79f1ee9c2cfe2d166aea47fb8a42a3e02fe5
Robert Rappoport <rjr363@nyu.edu>
2017-10-22 22:56:02 -0400

    added start screen

---
+++ Ghost Messages/Assets/TouchScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TouchScript : MonoBehaviour {
+	void Update() {
+		int fingerCount = 0;
+		foreach (Touch touch in Input.touches) {
+			if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
+				fingerCount++;
+
+		}
+		if (fingerCount > 0)
+			SceneManager.LoadScene ("basic");
+
+	}
+}
